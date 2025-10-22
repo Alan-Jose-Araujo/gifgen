17d4de29a3fec4c476d1a7c93a280b37fd9f2b4e
Alan José <alanjsdelima@gmail.com>
2025-10-22 00:37:58 -0300

    Adding CliArgumentParser implementation

---
+++ src/CliArgumentParser.cc
@@ -0,0 +1,19 @@
+/**
+ * CliArgumentParser implementation.
+ * @author Alan José <alanjsdelima@gmail.com>
+ */
+#include "CliArgumentParser.hh"
+
+CliArgumentParser::CliArgumentParser(const char* shortOptions, const std::vector<option>& longOptions)
+: shortOptions(shortOptions), longOptions(longOptions) {};
+
+void CliArgumentParser::parse(int argc, char* argv[], const std::function<void(int, const char*)>& callback) {
+    int cliOption;
+    std::vector<option> options = longOptions;
+    // 'getopt_long' function requires a C-style array, so add the terminator at the end.
+    options.push_back({nullptr, 0, nullptr, 0});
+
+    while((cliOption = getopt_long(argc, argv, shortOptions, options.data(), nullptr)) != -1) {
+        callback(cliOption, optarg);
+    }
+}
